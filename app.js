5763774d177d48c67615eaac91681547a5008938
aatifnehal786 <84582857+aatifnehal786@users.noreply.github.com>
2024-11-19 17:06:26 +0530

    Update app.js

--- app.js
+++ app.js
@@ -120,7 +120,7 @@ app.post("/forgot-password", async (req, res) => {
         await user.save();
 
         // Send email with reset link
-        const resetLink = `${process.env.RESET_LINK}/${token}`;
+        const resetLink = `https://nutrition-1-3.onrender.com/${token}`;
         const receiver = {
             from: process.env.MY_GMAIL,
             to: email,
